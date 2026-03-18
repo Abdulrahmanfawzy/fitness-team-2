d43500bad6a011a54aff060ce1eeab19dac451d7
ali-elmuzayan <ali.elmuzayn@gmail.com>
2026-03-18 21:52:48 +0200

    more change in the color

--- src/components/booking/PaymentAndConfirm.tsx
+++ src/components/booking/PaymentAndConfirm.tsx
@@ -10,7 +10,7 @@ const PaymentAndConfirm = ({
       <h1 className="font-bold text-3xl text-center mb-4">Payment & Confirm</h1>
 
       <div className="  bg-[#2D2D2D] rounded-md border border-gray-500 py-12 px-12 md:px-24">
-        <div className="max-w-3xl mx-auto border rounded border-gray-500 p-4 bg-black">
+        <div className="max-w-3xl mx-auto border rounded border-gray-500 p-4 bg-gray-950">
           <div className="flex flex-col items-center justify-center px-4 md:px-24">
             <h2 className="text-xl font-bold mb-4">Payment Method</h2>
             <div className="w-full">
